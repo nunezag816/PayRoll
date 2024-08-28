98bd366c5df0abb574def96d399e176bb668b65a
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-08-28 13:31:02 -0500

    Add skeleton for Payroll lab

---
+++ src/main/java/com/csc/Payroll.java
@@ -0,0 +1,8 @@
+package com.csc;
+
+public class Payroll {
+  // Implement your solution here!
+  public static void main(String[] args) {
+    System.out.println("Payroll!!!");
+  }
+}
---
+++ src/test/java/com/csc/TestPayroll.java
@@ -0,0 +1,15 @@
+package com.csc;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestPayroll {
+
+  Payroll payroll;
+
+  @BeforeEach
+  void setUp() {
+    payroll = new Payroll();
+  }
+
+  // Add your tests here!
+}
